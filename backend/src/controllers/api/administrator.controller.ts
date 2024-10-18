2b1575762bf7b888ad3c220f818f681b5b572e08
AleksandarUsljebrka <aleksandar.usljebrka@gmail.com>
2024-10-18 12:00:01 +0200

    Implemented api response mechanism

---
+++ backend/src/controllers/api/administrator.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
+import { Administrator } from "entities/administrator.entity";
+import { AddAdministratorDto } from "src/dtos/administrator/add.administrator.dto";
+import { EditAdministratorDto } from "src/dtos/administrator/edit.administrator.dto";
+import { ApiResponse } from "src/misc/api.response.class";
+import { AdministratorService } from "src/services/administrator/administrator.service";
+
+@Controller('api/administrator')
+export class AdministratorController{
+    constructor(
+        private administratorService:AdministratorService
+    ){}
+
+    @Get()
+    getAllAdmins():Promise<Administrator[]>{
+        return this.administratorService.getAll();
+    }
+   
+    @Get('/:id')
+    getById(@Param('id') administratorId:number):Promise<Administrator | ApiResponse>{
+        return this.administratorService.getById(administratorId);
+    }
+
+    @Post()
+    add(@Body() data:AddAdministratorDto):Promise<Administrator | ApiResponse>{
+        return this.administratorService.add(data);
+    }
+
+    @Put('/:id')
+    edit(@Param('id') id:number, @Body() data:EditAdministratorDto):Promise<Administrator | ApiResponse>{
+        return this.administratorService.editById(id,data);
+    }
+}
--- backend/src/services/administrator/administrator.service.ts
+++ backend/src/services/administrator/administrator.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { error } from 'console';
 import { Administrator } from 'entities/administrator.entity';
+import { resolve } from 'path';
+import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dto';
+import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { ApiResponse } from 'src/misc/api.response.class';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -13,7 +18,52 @@ export class AdministratorService {
     getAll():Promise<Administrator[]>{
         return this.administrator.find();
     }
-    getById(id:number):Promise<Administrator>{
-        return this.administrator.findOne({where:{administratorId:id}});
+    async getById(id:number):Promise<Administrator | ApiResponse>{
+        let admin:Administrator = await this.administrator.findOne({where:{administratorId:id}});
+        
+        if(admin === undefined || admin === null){
+            return new Promise(resolve => {
+                resolve(new ApiResponse('error', -1002));
+            });
+        }
+    }
+    add(data:AddAdministratorDto):Promise<Administrator | ApiResponse>{
+        const crypto = require('crypto');
+        
+        const passwordHash = crypto.createHash('sha512');
+        passwordHash.update(data.password);
+
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+        let newAdmin:Administrator = new Administrator();
+        newAdmin.username = data.username;
+        newAdmin.passwordHash = passwordHashString;
+
+        return new Promise((resolve)=>{
+            this.administrator.save(newAdmin)
+            .then(data => resolve(data))
+            .catch(error =>{
+                const response = new ApiResponse("error", -1001);
+                resolve(response);
+            })
+        })
+    }
+
+    async editById(id:number, data:EditAdministratorDto):Promise<Administrator | ApiResponse>{
+        let admin:Administrator = await this.administrator.findOne({where:{administratorId:id}});
+        
+        if(admin === undefined || admin === null){
+            return new Promise(resolve => {
+                resolve(new ApiResponse('error', -1002));
+            });
+        }
+        const crypto = require('crypto');
+        const passwordHash = crypto.createHash('sha512');
+        passwordHash.update(data.password);
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+        admin.passwordHash = passwordHashString;
+
+        return this.administrator.save(admin);
     }
 }
