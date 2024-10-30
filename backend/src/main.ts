7b5493e25c73fc16ba363111eba898e4bfb1029e
AleksandarUsljebrka <aleksandar.usljebrka@gmail.com>
2024-10-30 14:28:48 +0100

    Implemented cross origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,11 @@ async function bootstrap() {
     index:false
   })
 
+  app.enableCors({
+    origin: 'http://localhost:4000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
